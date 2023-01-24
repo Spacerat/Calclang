5bf51aa44d608adab9fc6d3ce08235316212348e
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-01-23 21:42:28 -0800

    fix API url

--- calc-frontend/app/page.tsx
+++ calc-frontend/app/page.tsx
@@ -7,7 +7,7 @@ import Results from "./Results";
 
 async function getResult(code: string): Promise<unknown> {
   const params = new URLSearchParams({ code });
-  const url = "https://walrus-app-wpbvo.ondigitalocean.app/compute/?" + params;
+  const url = "https://walrus-app-wpbvo.ondigitalocean.app/compute?" + params;
 
   const response = await fetch(url, { cache: "no-store" });
 
