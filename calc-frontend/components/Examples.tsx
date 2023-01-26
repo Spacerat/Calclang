8c5c32230d42a682e1e56a3aa2966350fd0bbfe3
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2023-01-26 00:59:15 -0800

    better examples

--- calc-frontend/components/Examples.tsx
+++ calc-frontend/components/Examples.tsx
@@ -2,7 +2,14 @@ import styles from "./AnalysisDisplay.module.css";
 
 const examples = [
   `10 days + 3 weeks`,
-  `10 .. 90 .. 100`,
+  `cleaning project cost = 500 to $600 / 2 to 3 days`,
+  `most often 90 = 10 .. 90 .. 100`,
+  `
+project time = (frontend + backend) / engineers
+frontend = 2 to 3 weeks
+backend = 3 to 4 weeks
+engineers = 2 to 3
+  `,
   `
 expenses per month = food per day * 30 + rent + utilities
 
@@ -11,12 +18,6 @@ rent = $1500
 utilities = $300 to $400
 
 expenses per month < 3000  
-`,
-  `
-project time = (frontend + backend) / engineers
-frontend = 2 to 3 weeks
-backend = 3 to 4 weeks
-engineers = 2 to 3
 `,
 ];
 
