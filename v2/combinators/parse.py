3da621624a5b2b2b119984900dd30353b765c00c
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2024-08-31 08:30:38 -0700

    Delete v2 directory

--- v2/combinators/parse.py
+++
@@ -1,8 +0,0 @@
-from .core import Parser, Result, ParserContext
-from typing import TypeVar
-
-T = TypeVar("T")
-
-
-def parse(parser: Parser[T], text: str) -> Result[T]:
-    return parser(ParserContext(text, 0))
