ef4e170bf71cc99a91ac8e9621512f7ee80b4252
Joseph Atkins-Turkish <spacerat3004@gmail.com>
2024-09-28 13:21:46 -0700

    analyse/parse commands

--- v0/app/local.py
+++ v0/app/local.py
@@ -1,11 +1,28 @@
 import sys
-from language.parse import parse_file
+from lib.language.analysis import DistributionOutput, analyse_result
+from lib.language.parse import parse_file
 from pprint import pprint
+import click
 
+@click.group()
+def cli():
+    pass
 
-def main(path):
-    pprint(parse_file(path))
+@cli.command()
+@click.argument('path', type=click.Path(exists=True))
+def analyse(path):
+    if last_result := parse_file(path).execute().last_result:
+        if analysis := analyse_result(last_result):
+            print(f"Analysis for {analysis.name}\n")
+            for output in analysis.outputs:
+                if not isinstance(output, DistributionOutput):
+                    pprint(output)
 
+@cli.command()
+@click.argument('path', type=click.Path(exists=True))
+def parse(path):
+    if parsed := parse_file(path):
+        pprint(parsed)
 
-if __name__ == "__main__":
-    main(sys.argv[1])
+if __name__ == '__main__':
+    cli()
